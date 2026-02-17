18f88c767c7bf7196442be9ab0ddd85d92c3335d
ramakarl <ramahoetzlein@gmail.com>
2026-02-16 16:05:49 -0800

    linux build fixes

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
+
+export CUDA_PATH=/usr/local/cuda-12.1
+export CUDA_ARCH=60
+# export LINUX_DEBUG
+
 cmake CMakeLists.txt \
     -DLIBMIN_ROOT=../libmin \
-    -B../build/Flock2 \
+    -Bbuild \
     -DBUILD_CUDA=true \
     -DBUILD_OPENGL=true \
     -DBUILD_GLEW=true
 
-make -C../build/Flock2
+make -Cbuild
